4c42522a5dee5055b878bb53527192f88c82bd66
David Cho <davidcho@usmba-2021p6251.westminster.net>
2021-08-24 17:03:08 -0400

    added shooter functions

--- src/main/java/frc/subsystems/Shooter.java
+++ src/main/java/frc/subsystems/Shooter.java
@@ -1,10 +1,20 @@
 package frc.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import com.revrobotics.CANSparkMax;
-
 import frc.robot.RobotMap;
 
 public class Shooter {
-    
+    //indicate left/right
+    DoubleSolenoid leftSolenoid; 
+    DoubleSolenoid rightSolenoid; 
+
+    public Shooter() {
+        leftSolenoid = new DoubleSolenoid(RobotMap.LEFT_INTAKE_EXTEND, RobotMap.LEFT_INTAKE_RETRACT);
+        rightSolenoid = new DoubleSolenoid(RobotMap.RIGHT_INTAKE_EXTEND, RobotMap.RIGHT_INTAKE_RETRACT);
+    }
+
+    public void shoot() {
+        leftSolenoid.set(DoubleSolenoid.Value.kForward);  
+        rightSolenoid.set(DoubleSolenoid.Value.kForward); 
+    }
 }
