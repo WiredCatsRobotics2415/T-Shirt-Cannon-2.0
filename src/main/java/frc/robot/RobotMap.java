aa3d1063f3a8ab8d86c489ca54ef9fc448b2780e
David Cho <davidcho@usmba-2021p6251.westminster.net>
2021-08-24 17:01:15 -0400

    added shooter functions

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,3 +1,7 @@
 public class RobotMap {
-    
+    //shooter penumatics
+    public static final int LEFT_INTAKE_EXTEND = 0;
+    public static final int LEFT_INTAKE_RETRACT = 0;
+    public static final int RIGHT_INTAKE_EXTEND = 0;
+    public static final int RIGHT_INTAKE_RETRACT = 0;
 }
---
+++ src/main/java/frc/subsystems/Shooter.java
@@ -0,0 +1,10 @@
+package frc.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import com.revrobotics.CANSparkMax;
+
+import frc.robot.RobotMap;
+
+public class Shooter {
+    
+}
